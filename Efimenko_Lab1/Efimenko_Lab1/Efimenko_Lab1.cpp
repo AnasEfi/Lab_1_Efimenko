4602a2ba9f9edb011f95c773078a17ec4ebfb398
AnasEfi <70594830+AnasEfi@users.noreply.github.com>
2020-09-16 15:30:17 +0300

    1

---
+++ Efimenko_Lab1/Efimenko_Lab1/Efimenko_Lab1.cpp
@@ -0,0 +1,28 @@
+﻿// Efimenko_Lab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct Tube
+{
+    string Name;
+    double score;
+    float price;
+
+};
+
+struct compressor
+{
+    std::string str;
+    string Name;
+    double score;
+    float price;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ Efimenko_lab1(2)/Efimenko_lab1(2)/Efimenko_lab1(2).cpp
@@ -0,0 +1,14 @@
+﻿// Efimenko_lab1(2).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h" //предкомпилированный заголовочный файл
+#include <iostream>
+
+void main()
+{
+    using namespace std;
+    cout <<"\'Hello World!\'"<< endl;
+    int i;
+    cin >> i;
+}
+
