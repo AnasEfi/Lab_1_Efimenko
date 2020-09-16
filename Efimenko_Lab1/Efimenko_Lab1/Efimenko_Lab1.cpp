e658131792049b27fa2e8d52ea227e1d546062f1
AnasEfi <70594830+AnasEfi@users.noreply.github.com>
2020-09-16 17:50:43 +0300

    Update Efimenko_Lab1.cpp

--- Efimenko_Lab1/Efimenko_Lab1/Efimenko_Lab1.cpp
+++ Efimenko_Lab1/Efimenko_Lab1/Efimenko_Lab1.cpp
@@ -8,21 +8,48 @@ using namespace std;
 struct Tube
 {
     string Name;
-    double score;
+    
     float price;
+    double diametr;
+    double length;
+    char *ID;
 
 };
 
-struct compressor
+/*struct compressor
 {
     std::string str;
     string Name;
     double score;
     float price;
-};
+};*/
+
+
+Tube CreateTube()
+   {
+    Tube bigTube;
+    setlocale(LC_ALL, "rus");
+    
+    cout << " Введите Имя трубы" << "\n";
+    cin >> bigTube.Name;
+    cout << "Введите Длину трубы"  << "\n";
+    cin >> bigTube.length;
+    cout << "Ведите Диаметр трубы" << "\n";
+    cin >> bigTube.diametr;
+    bigTube.ID = " ";
+   
+    return bigTube;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    Tube bigTube_1;
+    bigTube_1.ID = "1A";
+    CreateTube;
+    cout << bigTube_1;
+    return 0;
+    
 }
 
