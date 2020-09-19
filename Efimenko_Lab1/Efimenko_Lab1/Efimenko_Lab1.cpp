015770b1e98ecb81453d54cad88a7a4c94c513f4
AnasEfi <70594830+AnasEfi@users.noreply.github.com>
2020-09-19 21:14:10 +0300

    Bool/repair

--- Efimenko_Lab1/Efimenko_Lab1/Efimenko_Lab1.cpp
+++ Efimenko_Lab1/Efimenko_Lab1/Efimenko_Lab1.cpp
@@ -12,6 +12,8 @@ struct Tube
     double diametr;
     double length;
     string ID;
+    bool repair;
+    string help;
 };
 
 /*struct compressor
@@ -23,33 +25,43 @@ struct Tube
 };*/
 
 
- Tube CreateTube()
-   {
+Tube CreateTube()
+{
     Tube x;
     cout << "Введите идентификатор трубы" << "\n";
     cin >> x.ID;
-    cout << "Введите длину трубы в мм"  << "\n";
+    cout << "Введите длину трубы в мм" << "\n";
     cin >> x.length;
     cout << "Ведите диаметр трубы в мм" << "\n";
     cin >> x.diametr;
     ofstream outf("Text.txt", ios::app);
     outf << x.Name << "\n" << x.diametr << "\n" << x.length;
+    cout << "Труба находится в ремонте? Введите пожалуйста 0=нет или 1=да" << endl;
+    cin >> x.repair;
+    if (x.repair = true)
+    {
+        //x.repair = true;
+        cout << "Труба находится в ремонте" << endl;
+    }
+    else if (x.repair= false)
+    {
+       // x.repair = false;
+        cout << "Труба готова к использованию" << endl;
+    };
     return x;
 }
 
 
-int main()
+    int main()
 {
-    ifstream file;
-    file.open("Lab_1.txt");
+    /*ifstream file;
+    file.open("Lab_1.txt");*/
     setlocale(LC_ALL, "rus");
 
 Tube Tube1;
 Tube1.Name ="ту 14-161-173-97";
 cout << "Имя трубы  "<<Tube1.Name<<"\n";
     CreateTube();
-
-   
     return 0; 
     
 }
