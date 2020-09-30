55920266cd6f3a01de724dc4890c700618dda52a
AnasEfi <70594830+AnasEfi@users.noreply.github.com>
2020-09-30 17:59:32 +0300

    Menu

--- Efimenko_Lab1/Efimenko_Lab1/Efimenko_Lab1.cpp
+++ Efimenko_Lab1/Efimenko_Lab1/Efimenko_Lab1.cpp
@@ -4,9 +4,10 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include <vector>
 using namespace std;
 
-struct Tube
+struct Pipe
 {
     string Name;
     double diametr;
@@ -24,45 +25,140 @@ struct Tube
     float price;
 };*/
 
-
-Tube CreateTube()
+void PrintMenu()
 {
-    Tube x;
-    cout << "Введите идентификатор трубы" << "\n";
-    cin >> x.ID;
-    cout << "Введите длину трубы в мм" << "\n";
+    cout << "1.Создать трубу" << endl
+        << "2.Загрузить трубу из файла" << endl
+        << "3.Просмотреть данные о трубе" << endl
+        << "4.Вывести данные о трубе в файл" << endl
+        << "5.Изменить статус трубы (в ремонте)" << endl
+
+        << "6.Создать КС" << endl
+        << "7.Сохранить данные о КС в файл" << endl
+        << "8.Просмотреть данные о КС" << endl
+        << "9.Изменить количество работающих цехов" << endl
+        << "0.Выход" << endl;
+}
+Pipe CreatePipe()
+{
+    Pipe x;
+
+    cout << "Введите пожалуйста имя трубы в мм: ";
+    cin >> x.Name;
+
+    cout << "Введите пожалуйста длину трубы в мм: ";
     cin >> x.length;
-    cout << "Ведите диаметр трубы в мм" << "\n";
+
+    cout << "Введите пожалуйста диаметр трубы в мм: ";
     cin >> x.diametr;
-    ofstream outf("Text.txt", ios::app);
-    outf << x.Name << "\n" << x.diametr << "\n" << x.length;
-    cout << "Труба находится в ремонте? Введите пожалуйста 0=нет или 1=да" << endl;
+
+    /*cout << "Труба находится в ремонте? Введите пожалуйста 0=нет или 1=да" << endl;
     cin >> x.repair;
-    if (x.repair = true)
+    if (x.repair = true);
     {
-        //x.repair = true;
         cout << "Труба находится в ремонте" << endl;
     }
-    else if (x.repair= false)
+    else if (x.repair = false);
     {
-       // x.repair = false;
         cout << "Труба готова к использованию" << endl;
-    };
+    };*/
+    return x;
+};
+Pipe LoadPipe()
+{
+    ifstream fin;
+    Pipe x;
+    fin.open("Lab_1.txt", ios::in);
+    if (fin.is_open())
+    {
+       
+        fin >> x.Name;
+        fin >> x.diametr;
+        fin >> x.length;
+        fin >> x.repair;
+        fin >> x.ID;
+        fin.close();
+    }
+    return x;
+} 
+void PipeEdit(bool& status)
+{
+
+    cout << "Изменить значение: Труба находится в ремонте?" << "\n" << "Введите пожалуйста 0 = нет или 1 = да" << endl;
+    cin >> status;
+    if (status == true)
+    {
+
+        cout << "Труба находится в ремонте" << endl;
+    }
+    else if (status == false)
+    {
+
+        cout << "Труба готова к использованию" << endl;
+    }
+}
+   
+Pipe PrintPipe(Pipe x)
+{
+    cout << "Имя трубы: " << x.Name << endl
+        << "Длина трубы: " << x.length << endl
+        << "Диаметр трубы: " << x.diametr << endl
+        << "Статус трубы (в ремонте): " << x.repair << endl;
     return x;
 }
-
-
-    int main()
+void SavePipe(const Pipe& x)
 {
-    /*ifstream file;
-    file.open("Lab_1.txt");*/
+
+    ofstream outf("Text.txt", ios::app);
+    outf << "Имя трубы: " << x.Name << "\n" << "Диаметр трубы: " << x.diametr << "\n" << "Длина трубы: " << x.length << endl;
+    outf << "Труба находится в ремонте:" << x.repair;
+}
+int main()
+
+{
     setlocale(LC_ALL, "rus");
 
-Tube Tube1;
-Tube1.Name ="ту 14-161-173-97";
-cout << "Имя трубы  "<<Tube1.Name<<"\n";
-    CreateTube();
-    return 0; 
-    
+    while (1)
+    {
+        PrintMenu();
+        int i = 0;
+        cin >> i;
+        Pipe x;
+        switch (i)
+        {
+        case 1:
+        {
+            x = CreatePipe();
+            break;
+        }
+        case 2:
+        {
+            x = LoadPipe();
+            break;
+        }
+        case 3:
+        {
+            break;
+        }
+        case 4:
+        {
+            break;
+        }
+        case 5:
+        {
+            PipeEdit();
+            break;
+        }
+        case 0:
+        { return 0;
+        break;
+        }
+        default:
+        {
+            cout << "Error" << endl;
+        }
+        }
+        return 0;
+    }
 }
 
